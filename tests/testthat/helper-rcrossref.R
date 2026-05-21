40ce47c2cf6c749999e4a9fcb883836b00e867e9
chreman <web@christopherkittel.eu>
2026-05-21 17:05:17 +0200

    API token helper functions

--- tests/testthat/helper-rcrossref.R
+++ tests/testthat/helper-rcrossref.R
@@ -3,7 +3,8 @@ library("vcr")
 invisible(vcr::vcr_configure(
     dir = "../fixtures",
     filter_sensitive_data = list(
-        "<crossref_email>" = Sys.getenv("crossref_email")
+        "<crossref_email>" = Sys.getenv("crossref_email"),
+        "<crossref_plus_token>" = Sys.getenv("crossref_plus_token")
     )
 ))
 
