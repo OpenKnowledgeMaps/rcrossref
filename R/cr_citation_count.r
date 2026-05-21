4e117416126a97c970d2360b699e630af242eeda
chreman <web@christopherkittel.eu>
2026-05-21 17:04:31 +0200

    add new function for async citation requests from new crossref REST API endpoint

--- R/cr_citation_count.r
+++ R/cr_citation_count.r
@@ -1,80 +1,118 @@
-#' Get a citation count via CrossRef OpenURL
+#' Get a citation count via CrossRef
 #'
 #' @export
 #'
-#' @param doi (character) One or more digital object identifiers. If 
-#' `async=FALSE` we do synchronous HTTP requests in an `lapply` call, but 
-#' if `async=TRUE`, we do asynchronous HTTP requests.
-#' @param url (character) the url for the function (should be left to default)
-#' @param key your Crossref OpenURL email address, either enter, or loads
-#' from `.Rprofile`. We use a default, so you don't need to pass this.
-#' @param async (logical) use async HTTP requests. Default: `FALSE`
-#' @param ... Curl options passed on to [crul::HttpClient()]
+#' @param doi (character) One or more digital object identifiers.
+#' @param url (character) the url for the OpenURL function (used when no Plus
+#'   token is configured)
+#' @param key your Crossref OpenURL email address. Used as the `pid` parameter
+#'   for the legacy OpenURL API when `plus_token` is not set.
+#' @param async (logical) use async HTTP requests for the legacy OpenURL path.
+#'   Default: `FALSE`. Ignored when `plus_token` is set.
+#' @param plus_token (character) Crossref Metadata Plus API token. When set,
+#'   uses the REST API (`https://api.crossref.org/works/{doi}`) with the
+#'   `Crossref-Plus-API-Token: Bearer <token>` header and sends requests in
+#'   async batches. Defaults to the `crossref_plus_token` environment variable.
+#' @param batch_size (integer) number of concurrent async requests per batch
+#'   when using the Plus REST path. Defaults to the `crossref_batch_size`
+#'   environment variable, or 50 if unset.
+#' @param ... Curl options passed on to [crul::HttpClient()] or
+#'   [crul::HttpRequest()]
 #'
-#' @return a data.frame, with columns `doi` and `count`. The count column 
-#' has numeric values that are the citation count for that DOI, or `NA` if 
-#' not found or no count available
-#' 
-#' @details See https://www.crossref.org/labs/openurl/ for more info on this
-#' Crossref API service.
+#' @return a data.frame with columns `doi` and `count`. `count` is numeric or
+#'   `NA` when not found.
 #'
-#' This number is also known as **cited-by**
+#' @details
+#' When `plus_token` is provided (or set via the `crossref_plus_token` env
+#' var), the function uses the Crossref REST API with your Metadata Plus
+#' subscription, which provides higher rate limits. Requests are sent in
+#' async batches of `batch_size` concurrent connections.
 #'
-#' Note that this number may be out of sync/may not match that that the
-#' publisher is showing (if they show it) for the same DOI/article.
-#'
-#' We've contacted Crossref about this, and they have confirmed this.
-#' Unfortunately, we can not do anything about this.
-#'
-#' I would imagine it's best to use this data instead of from the publishers,
-#' and this data you can get programatically :)
-#' 
-#' @section failure behavior:
-#' When a DOI does not exist, we may not get a proper HTTP status code 
-#' to throw a proper stop status, so we grep on the text itself, and throw
-#' a stop if only one DOI passed and not using async, or warning if more
-#' than one DOI passed or if using async.
+#' Without a Plus token the legacy OpenURL API is used
+#' (`https://doi.crossref.org/openurl/`). Set `async = TRUE` to fire all
+#' OpenURL requests concurrently (no batching).
 #'
 #' @seealso [cr_search()], [cr_r()]
-#' @author Carl Boettiger \email{cboettig@@gmail.com}, 
-#' Scott Chamberlain
+#' @author Carl Boettiger \email{cboettig@@gmail.com}, Scott Chamberlain
 #' @examples \dontrun{
-#' cr_citation_count(doi="10.1371/journal.pone.0042793")
-#' cr_citation_count(doi="10.1016/j.fbr.2012.01.001")
-#' ## many
+#' cr_citation_count(doi = "10.1371/journal.pone.0042793")
+#'
+#' # Many DOIs, legacy async
 #' dois <- c("10.1016/j.fbr.2012.01.001", "10.1371/journal.pone.0042793")
+#' cr_citation_count(doi = dois, async = TRUE)
+#'
+#' # Metadata Plus path (token read from crossref_plus_token env var)
+#' Sys.setenv(crossref_plus_token = "my-token")
 #' cr_citation_count(doi = dois)
-#' # DOI not found
-#' cr_citation_count(doi="10.1016/j.fbr.2012")
-#' 
-#' # asyc
-#' dois <- c("10.1016/j.fbr.2012.01.001", "10.1371/journal.pone.0042793", 
-#'  "10.1016/j.fbr.2012", "10.1109/tsp.2006.874779", "10.1007/bf02231542", 
-#'  "10.1007/s00277-016-2782-z", "10.1002/9781118339893.wbeccp020", 
-#'  "10.1177/011542659200700105", "10.1002/chin.197444438", 
-#'  "10.1002/9781118619599.ch4", "10.1007/s00466-012-0724-8", 
-#'  "10.1017/s0376892900029477", "10.1167/16.12.824")
-#' res <- cr_citation_count(doi = dois, async = TRUE)
-#' ## verbose curl
-#' res <- cr_citation_count(doi = dois, async = TRUE, verbose = TRUE)
-#' res
-#' ## time comparison
-#' system.time(cr_citation_count(doi = dois, async = TRUE))
-#' system.time(cr_citation_count(doi = dois, async = FALSE))
-#' 
-#' # from a set of random DOIs
-#' cr_citation_count(cr_r(50), async = TRUE)
+#'
+#' # Explicit token and batch size
+#' cr_citation_count(doi = dois, plus_token = "my-token", batch_size = 25L)
 #' }
 
-cr_citation_count <- function(doi, url = "http://www.crossref.org/openurl/",
-	key = "cboettig@ropensci.org", async = FALSE, ...) {
+cr_citation_count <- function(doi,
+    url = "http://www.crossref.org/openurl/",
+    key = "cboettig@ropensci.org",
+    async = FALSE,
+    plus_token = Sys.getenv("crossref_plus_token"),
+    batch_size = as.integer(
+      Sys.getenv("crossref_batch_size", unset = "50")),
+    ...) {
 
-  if (async) {
+  if (nchar(plus_token) > 0) {
+    cr_cc_rest_batched(doi, plus_token = plus_token, batch_size = batch_size, ...)
+  } else if (async) {
     cr_cc_async(doi, url, key, ...)
   } else {
     out <- lapply(doi, cr_cc, ur = url, key = key, ...)
     data.frame(doi = doi, count = unlist(out), stringsAsFactors = FALSE)
   }
+}
+
+# REST API path: send one batch of DOIs concurrently, return data.frame
+cr_cc_rest_async <- function(doi, plus_token, ...) {
+  headers <- list(
+    `User-Agent` = rcrossref_ua(),
+    `X-USER-AGENT` = rcrossref_ua(),
+    `Crossref-Plus-API-Token` = paste0("Bearer ", plus_token)
+  )
+  reqs <- lapply(doi, function(d) {
+    endpoint_url <- sprintf(
+      "https://api.crossref.org/works/%s",
+      utils::URLencode(d, reserved = TRUE)
+    )
+    crul::HttpRequest$new(
+      url = endpoint_url,
+      headers = headers,
+      opts = list(...)
+    )$get(query = list(select = "DOI,is-referenced-by-count"))
+  })
+  cli <- crul::AsyncVaried$new(.list = reqs)
+  cli$request()
+  cli$status()
+  cli$responses()
+  bodies <- cli$parse()
+  results <- mapply(function(txt, d) {
+    tryCatch({
+      r <- jsonlite::fromJSON(txt)
+      data.frame(
+        doi = d,
+        count = as.numeric(r$message$`is-referenced-by-count`),
+        stringsAsFactors = FALSE
+      )
+    }, error = function(e) {
+      warning("Failed to get citation count for DOI: ", d, call. = FALSE)
+      data.frame(doi = d, count = NA_integer_, stringsAsFactors = FALSE)
+    })
+  }, bodies, doi, SIMPLIFY = FALSE)
+  do.call(rbind, results)
+}
+
+# Splits DOIs into batches and calls cr_cc_rest_async for each
+cr_cc_rest_batched <- function(doi, plus_token, batch_size = 50L, ...) {
+  if (is.na(batch_size) || batch_size < 1L) batch_size <- 50L
+  batches <- split(doi, ceiling(seq_along(doi) / batch_size))
+  results <- lapply(batches, cr_cc_rest_async, plus_token = plus_token, ...)
+  do.call(rbind, results)
 }
 
 cr_cc <- function(doi, url, key, ...) {
